94cc8860f91539b97b031691739fdf74ada53b3c
alvinolubis <60283358+alvinolubis@users.noreply.github.com>
2020-10-04 07:08:08 +0700

    Add files via upload

---
+++ MVC/app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+
+	<h1 class="mt-3">About Me</h1>
+	<img src="<?= BASEURL;?>/IMG/img1.jpg" alt="Alvino Raditya" width="200">
+	<p>Halo, nama saya <?= $data['nama']?>, umur saya <?= $data['umur']?> tahun, saya adalah seorang <?= $data['pekerjaan']?>.</p>
+
+
+</div>
